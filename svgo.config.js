9eb5d89d58a244eed02fa5e6adc7a86770276861
BrieucCaillot <brieucdeveloper@gmail.com>
2023-04-02 16:18:21 +0200

    feat: :bento: Added new logos

--- svgo.config.js
+++ svgo.config.js
@@ -3,13 +3,19 @@ module.exports = {
 	multipass: true, // boolean. false by default
 	js2svg: {
 		indent: 2, // string with spaces or number of spaces. 4 by default
-		multipass: true,
 		pretty: true, // boolean, false by default
-		removeXMLNS: true,
 	},
 	plugins: [
-		'preset-default',
+		{
+			name: 'preset-default',
+			params: {
+				overrides: {
+					mergePaths: false,
+				},
+			},
+		},
 		'prefixIds',
+		'removeDimensions',
 		{
 			name: 'sortAttrs',
 			params: {
